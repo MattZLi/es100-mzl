a27ef45cd848a841380cc24ac580385acc2a166a
Matthew Li <matthewzli97@gmail.com>
2019-03-18 17:55:48 -0400

    added periodic recalibration

--- FDC_classify_testing/FDC_classify_testing.ino
+++ FDC_classify_testing/FDC_classify_testing.ino
@@ -36,32 +36,8 @@ uint8_t FDC_RD_M[]   = {1, 0, 3, 2, 5, 4, 7, 6}; // Addr's to read meas. value f
 bool    toggle       = true;
 float   gain         = (15.0) / (8388608.0);  // Nominal Gain in units of (pf / count)
 
-// create 4 queues of integers corresponding with MEASx
-QueueArray <float> queueA;
-QueueArray <float> queueB;
-QueueArray <float> queue3;
-QueueArray <float> queue4;
-
 // define length of queue
-//int queue_length = 10;
 int queue_length = 5;
-
-// create 4 floats to track the sum of each queue
-float filteredA = 0;
-float filteredB = 0;
-float sum3 = 0;
-float sum4 = 0;
-
-// create 4 ints to store threshold of touch
-// should be more like cap value * (float) queue_length
-//float threshold1 = 70;
-//float threshold2 = 60;
-//float threshold3 = 45;
-//float threshold4 = 45;
-float threshold1 = 5 * (float) queue_length;
-float threshold2 = 5 * (float) queue_length;
-float threshold3 = 6 * (float) queue_length;
-float threshold4 = 4.5 * (float) queue_length;
 
 // min and max
 float minA = (float) INT_MAX;
@@ -69,33 +45,23 @@ float maxA = (float) 0.0;
 float minB = (float) INT_MAX;
 float maxB = (float) 0.0;
 
+unsigned long last_t = 0;
+
+float temp_minA = (float) INT_MAX;
+float temp_maxA = (float) 0.0;
+float temp_minB = (float) INT_MAX;
+float temp_maxB = (float) 0.0;
+
+
 // normalized
 float normA = (float) 0.0;
 float normB = (float) 0.0;
+float pos_vert = (float) 0.0;
+float mag = (float) 0.0;
 
-// create 4 bools to track whether sensor is touched or not
-int touchA = 0;
-int touchB = 0;
-int touch3 = 0;
-int touch4 = 0;
+float capA = (float) 0.0;
+float capB = (float) 0.0;
 
-int prev_touchA;
-int prev_touchB;
-
-signed long riseA = 0;
-signed long fallA = 0;
-signed long riseB = 0;
-signed long fallB = 0;
-
-QueueArray <int> up_queue;
-QueueArray <int> down_queue;
-QueueArray <int> state_queue;
-
-int state = 0;
-int prev_state = 0;
-
-
-int pos;
 
 // ****************************************************************************************
 // Initialize FDC1004 device
@@ -103,7 +69,7 @@ int pos;
 void setup() {
   Wire.begin();              // join i2c bus (address optional for master)
   Serial.begin(115200);      // start serial communication at 115200 bps
-
+  
   write16(FDC_CONFIG, 0x8000);  // Issue Reset Command to chip
 
   // Setup the MEASx Registers for single ended measurements, CINx to MEASx
@@ -119,35 +85,6 @@ void setup() {
   //     all 4 MEASx's enabled.
   //
   write16(FDC_CONFIG, 0x0DF0);      //D = 400Hz, 5 = 100Hz
-
-  // enqueue each queue full of 0s
-  for (int i = 0; i < queue_length; i++) {
-    queueA.enqueue(0);
-    queueB.enqueue(0);
-    queue3.enqueue(0);
-    queue4.enqueue(0);
-  }
-
-  for (int i = 0; i < 4; i++) {
-    state_queue.enqueue(0);
-  }
-  Serial.print(state_queue.count());
-
-  // up: 0, 1, 3, 2
-  up_queue.enqueue(0);
-  up_queue.enqueue(1);
-  up_queue.enqueue(3);
-  up_queue.enqueue(2);
-
-  Serial.print(up_queue.count());
-
-  // down: 0, 2, 3, 1
-  down_queue.enqueue(0);
-  down_queue.enqueue(2);
-  down_queue.enqueue(3);
-  down_queue.enqueue(1);
-
-  Serial.print(down_queue.count());
   
 }
 
@@ -173,7 +110,8 @@ void read_meas(bool toggle) {
   float    cap = 0;    // floating point value of measured capacitance (in pf)
 
   int j = 0;
-  for (int i = 0; i <= 3; i++) {
+  for (int i = 0; i <= 1; i++) {
+//  for (int i = 0; i <= 3; i++) {
     lsb = read16(FDC_RD_M[j++]);     // MEASj LSB
     msb = read16(FDC_RD_M[j++]);     // MEASj MSB
     val = ((msb << 16) + lsb) >> 8;  // 24 bit combined MSB and LSB
@@ -186,140 +124,75 @@ void read_meas(bool toggle) {
         case 0:
           // add to array of cap values
           // if array is full, shift the window like a FIFO queue
-          
-          minA = min(minA, cap);
-          maxA = max(maxA, cap);
 
-          normA = (cap - minA)/(maxA - minA);
-//          Serial.print("normA");
-//          Serial.print(" ");
-//          Serial.print(normA);
-//          Serial.print(" ");
+          capA = cap;
+          
+          
+          
+
+          minA = min(minA, capA);
+          maxA = max(maxA, capA);
+          temp_minA = min(temp_minA, capA);
+          temp_maxA = max(temp_maxA, capA);
+
+          normA = (capA - minA)/(maxA - minA);
+          
           
           break;
         case 1:
+          capB = cap;
+          
 
-          minB = min(minB, cap);
-          maxB = max(maxB, cap);
+          minB = min(minB, capB);
+          maxB = max(maxB, capB);
+          temp_minB = min(temp_minB, capB);
+          temp_maxB = max(temp_maxB, capB);
 
-          normB = (cap - minB)/(maxB - minB);
-//          Serial.print("normB");
-//          Serial.print(" ");
-//          Serial.print(normB);
-//          Serial.print(" ");
+          normB = (capB - minB)/(maxB - minB);
+          
+          
           
           break;
         case 2:
-//          Serial.print("sensor3");
-//          Serial.print(" ");
-//          Serial.print(cap);
-//          Serial.print(" ");
-          sum3 = sum3 - queue3.dequeue() + cap;
-          queue3.enqueue(cap);
-          if (sum3 > threshold3) {
-            touch3 = 3;
-          } else {
-            touch3 = 0;
-          }
-//          Serial.print(touch3);
-//          Serial.print(" ");
+
           break;
         case 3:
-//          Serial.print("sensor3");
-//          Serial.print(" ");
-//          Serial.print(cap);
-//          Serial.print(" ");
-          sum4 = sum4 - queue4.dequeue() + cap;
-          queue4.enqueue(cap);
-          if (sum4 > threshold4) {
-            touch4 = 4;
-          } else {
-            touch4 = 0;
-          }
-//          Serial.print(touch4);
-//          Serial.print(" ");
+
           break;
         default:
           break;
       }
 
-      switch(state) {
-        case 0: // A = 0, B = 0
-          if (touchA == 1 && prev_touchA == 0) { // rising edge on A
-            state = 2;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          if (touchB == 1 && prev_touchB == 0) { // rising edge on B
-            state = 1;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          break;
-    
-        case 1: // A = 0, B = 1
-          if (touchA == 1 && prev_touchA == 0) { // rising edge on A
-            state = 3;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          if (touchB == 0 && prev_touchB == 1) { // falling edge on B
-            state = 0;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          break;
-        
-        case 2: // A = 1, B = 0
-          if (touchA == 0 && prev_touchA == 1) { // falling edge on A
-            state = 0;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          if (touchB == 1 && prev_touchB == 0) { // rising edge on B
-            state = 3;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          break;
-    
-        case 3: // A = 1, B = 1
-          if (touchA == 0 && prev_touchA == 1) { // falling edge on A
-            state = 1;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          if (touchB == 0 && prev_touchB == 1) { // falling edge on B
-            state = 2;
-//            Serial.print("state: ");
-//            Serial.print(state);
-            state_queue.enqueue(state);
-            state_queue.dequeue();
-          }
-          break;
-    
-        default:
-          break;
-      }
+      // Serial.print("capA");
+      // Serial.print(" ");
+      // Serial.print(capA);
+      // Serial.print(" ");
 
-      float pos_vert = (normA - normB + 1.0)/2.0;
+      // Serial.print("capB");
+      // Serial.print(" ");
+      // Serial.print(capB);
+      // Serial.print(" ");
+
+      // Serial.print("normA");
+      // Serial.print(" ");
+      // Serial.print(normA);
+      // Serial.print(" ");
+
+      // Serial.print("normB");
+      // Serial.print(" ");
+      // Serial.print(normB);
+      // Serial.print(" ");
+
+      pos_vert = (normA - normB + 1.0)/2.0;
       Serial.print("pos_vert");
       Serial.print(" ");
       Serial.print(pos_vert);
+      Serial.print(" ");
+
+      mag = (normA + normB)/2.0 - 2.0;
+      Serial.print("mag");
+      Serial.print(" ");
+      Serial.print(mag);
       Serial.print(" ");
 
       Serial.print("center");
@@ -327,40 +200,38 @@ void read_meas(bool toggle) {
       Serial.print(0.5);
       Serial.print(" ");
 
-//      Serial.print("riseA - riseB");
-      int delta_t = abs(riseA - riseB);
-//      Serial.print((riseA - riseB));
-//      Serial.println();
-//      if (((riseB - riseA) < 100) && ((riseB - riseA) > 0)) {
-      if ((delta_t < 100) && (delta_t > 0)) {
-//        Serial.print("TOUCH");
-//        Mouse.click(MOUSE_LEFT);
-        riseA = 0;
-        riseB = 0;
-        // clear buffer
-        state_queue.enqueue(0);
-        state_queue.dequeue();
-      } else if (QueueEqual(&state_queue, &up_queue)) {
-//         Serial.print("UP");
-//         Keyboard.write(KEY_UP_ARROW);
-         state_queue.enqueue(0);
-         state_queue.dequeue();
-      } else if (QueueEqual(&state_queue, &down_queue)) {
-//         Serial.print("DOWN");
-//         Keyboard.write(KEY_DOWN_ARROW);
-         state_queue.enqueue(0);
-         state_queue.dequeue();
+      Serial.print("top");
+      Serial.print(" ");
+      Serial.print(1.0);
+      Serial.print(" ");
+
+      Serial.print("bot");
+      Serial.print(" ");
+      Serial.print(0.0);
+      Serial.print(" ");
+
+      Serial.println();
+
+      unsigned long curr_t = millis();
+      // refresh mins and maxes every 3 seconds
+      if ((curr_t - last_t) > 3000) {
+        last_t = curr_t;
+        maxA = (maxA + temp_maxA)/2.0;
+        minA = (minA + temp_minA)/2.0;
+        maxB = (maxB + temp_maxB)/2.0;
+        minB = (minB + temp_minB)/2.0;
+        
+        temp_minA = (float) INT_MAX;
+        temp_maxA = (float) 0.0;
+        temp_minB = (float) INT_MAX;
+        temp_maxB = (float) 0.0;
       }
 
-      // update prev_touch
-      prev_touchA = touchA;
-  
-      // update prev_touch
-      prev_touchB = touchB;
     }
+    
   }
 
-  Serial.println();
+  // Serial.println();
 }
 
 // **********  Read/Write 16 bit value from an FDC1004 register *******************
@@ -412,6 +283,26 @@ bool QueueEqual(QueueArray <int>* A, QueueArray <int>* B) {
   }
   return true;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
